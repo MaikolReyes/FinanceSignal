0a82e8b0109179920e2d1c7eedb97fe260b13548
Maikol Reyes <Maikol.Reyes@somosrex.com>
2024-08-26 19:46:46 -0300

    cambio en app

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { Navbar, Header, Footer } from "./layout"
 export const App = () => {
   return (
     <>
-      <h1 className="text-center mt-10">FinanceSignal</h1>
+      <h1 className="text-center mt-10">FinanceSignal xdxdxd</h1>
       <Navbar />
       <Header />
       <Footer />
