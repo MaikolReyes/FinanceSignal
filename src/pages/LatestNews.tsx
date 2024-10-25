aae5ebc5ad50252dd1765df34548fb9540f4f0cc
Maikol Reyes <Maikol.Reyes@somosrex.com>
2024-10-25 17:00:09 -0300

    Add Footer Component

--- src/pages/LatestNews.tsx
+++ src/pages/LatestNews.tsx
@@ -4,8 +4,6 @@ export const LatestNews = () => {
   return (
     <>
       <div className="grid grid-cols-2 p-3 justify-items-center">
-
-
         <div className="articlesPrincipal w-full">
           <div id="carouselExampleCaptions" className="carousel slide">
             <div className="carousel-indicators">
@@ -86,67 +84,93 @@ export const LatestNews = () => {
               <span className="visually-hidden">Next</span>
             </button>
           </div>
-
-
         </div>
 
         <div className="w-4/5 max-h-72 grid grid-cols-2 gap-3 mt-0">
-
           <div className="">
             <img src={foto} className="card-img-top" alt="foto" />
             <div className="card-body">
-              <h5 className="card-title mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+              <h5 className="card-title mb-0">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </h5>
             </div>
           </div>
 
           <div className="">
             <img src={foto} className="card-img-top" alt="foto" />
             <div className="card-body">
-              <h5 className="card-title mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+              <h5 className="card-title mb-0">
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </h5>
             </div>
           </div>
 
           <div className="mt-2">
             <div className="card-body">
-              <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+              <h6 className="card-subtitle mb-2 text-body-secondary">
+                Card subtitle
+              </h6>
               <h5 className="card-title">Card title</h5>
             </div>
           </div>
 
           <div className="mt-2">
             <div className="card-body">
-              <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+              <h6 className="card-subtitle mb-2 text-body-secondary">
+                Card subtitle
+              </h6>
               <h5 className="card-title">Card title</h5>
             </div>
           </div>
-
         </div>
-
       </div>
 
       <div className="w-full flex gap-5 p-3">
         <div className="">
           <img src={foto} className="card-img-top" alt="foto" />
           <div className="card-body">
-            <h5 className="card-title mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+            <h5 className="card-title mb-0">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </h5>
           </div>
         </div>
         <div className="">
           <img src={foto} className="card-img-top" alt="foto" />
           <div className="card-body">
-            <h5 className="card-title mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+            <h5 className="card-title mb-0">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </h5>
           </div>
         </div>
         <div className="">
           <img src={foto} className="card-img-top" alt="foto" />
           <div className="card-body">
-            <h5 className="card-title mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+            <h5 className="card-title mb-0">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </h5>
           </div>
         </div>
         <div className="">
           <img src={foto} className="card-img-top" alt="foto" />
           <div className="card-body">
-            <h5 className="card-title mb-0">Some quick example text to build on the card title and make up the bulk of the card's content.</h5>
+            <h5 className="card-title mb-0">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </h5>
+          </div>
+        </div>
+        <div className="">
+          <img src={foto} className="card-img-top" alt="foto" />
+          <div className="card-body">
+            <h5 className="card-title mb-0">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </h5>
           </div>
         </div>
       </div>
@@ -155,11 +179,7 @@ export const LatestNews = () => {
         <div className="card">
           <div className="row g-0">
             <div className="col-md-4">
-              <img
-                src={foto}
-                className="img-fluid rounded-start"
-                alt="foto"
-              />
+              <img src={foto} className="img-fluid rounded-start" alt="foto" />
             </div>
 
             <div className="col-md-8">
@@ -183,11 +203,53 @@ export const LatestNews = () => {
         <div className="card">
           <div className="row g-0">
             <div className="col-md-4">
-              <img
-                src={foto}
-                className="img-fluid rounded-start"
-                alt="foto"
-              />
+              <img src={foto} className="img-fluid rounded-start" alt="foto" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Noticia 2</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={foto} className="img-fluid rounded-start" alt="foto" />
+            </div>
+            <div className="col-md-8">
+              <div className="card-body">
+                <h5 className="card-title">Noticia 2</h5>
+                <p className="card-text">
+                  This is a wider card with supporting text below as a natural
+                  lead-in to additional content. This content is a little bit
+                  longer.
+                </p>
+                <p className="card-text">
+                  <small className="text-body-secondary">
+                    Last updated 3 mins ago
+                  </small>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="card">
+          <div className="row g-0">
+            <div className="col-md-4">
+              <img src={foto} className="img-fluid rounded-start" alt="foto" />
             </div>
             <div className="col-md-8">
               <div className="card-body">
