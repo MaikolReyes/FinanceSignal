eeab89010f291968d550c8d1f2369253e1f7be8f
Maikol Reyes <maikolreyes209@gmail.com>
2025-03-01 14:17:33 -0300

    changes in carousel text

--- src/home/CarouselSlider.tsx
+++ src/home/CarouselSlider.tsx
@@ -18,7 +18,7 @@ export const CarouselSlider = () => {
                                 <Link to={`/article/${title}`} className="no-underline text-inherit position-relative">
 
                                     <img src={cover} loading="lazy" className="object-fit-cover w-full h-[250px] desktop:h-[450px] large-desktop:h-[500px]" alt="Noticias Recientes" />
-                                    <div className="position-absolute bottom-10 left-[10%] right-[10%] desktop:bottom-12 p-3 flex flex-col items-center text-white text-center bg-black bg-opacity-50 rounded-lg ">
+                                    <div className="position-absolute bottom-4 left-[15%] right-[15%] tablet:bottom-8 tablet:left-[10%] tablet:right-[10%] p-3 flex flex-col items-center text-white text-center bg-black bg-opacity-50 rounded-lg ">
                                         <h5 className="font-title text-base desktop:text-lg large-desktop:text-xl">{title}</h5>
                                         <div className="truncated-text text-sm desktop:text-base large-desktop:text-lg text-gray-300">
                                             <BlocksRenderer content={contenido} />
@@ -35,7 +35,7 @@ export const CarouselSlider = () => {
                     data-bs-target="#carouselExampleCaptions"
                     data-bs-slide="prev">
                     <span
-                        className="carousel-control-prev-icon bg-black rounded-md"
+                        className="carousel-control-prev-icon "
                         aria-hidden="true"
                     ></span>
                     <span className="visually-hidden">Previous</span>
@@ -47,7 +47,7 @@ export const CarouselSlider = () => {
                     data-bs-slide="next"
                 >
                     <span
-                        className="carousel-control-next-icon bg-black rounded-md"
+                        className="carousel-control-next-icon "
                         aria-hidden="true"
                     ></span>
                     <span className="visually-hidden">Next</span>
