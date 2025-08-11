763694f4f7657a5cf3fb0a0fee375cc5fb404a12
Maikol Reyes <maikolreyes209@gmail.com>
2025-08-10 21:18:28 -0300

    change in links slug

--- src/components/SideNews.tsx
+++ src/components/SideNews.tsx
@@ -6,7 +6,7 @@ export const SideNews = () => {
 
     return (
         <div>
-            {recentArticles.slice(3, 5).map(({ title, id, publishedAt }) => {
+            {recentArticles.slice(3, 5).map(({ title, id, publishedAt,slug }) => {
 
                 const formattedDate = new Intl.DateTimeFormat('es-ES', {
                     year: 'numeric',
@@ -17,7 +17,7 @@ export const SideNews = () => {
                 return (
 
                     <div key={id} className='w-2/3'>
-                        <Link to={`/article/${title}`} className="w-full no-underline text-inherit">
+                        <Link to={`/article/${slug}`} className="w-full no-underline text-inherit">
                             <div className="card w-full">
                                 <div className="card-body">
                                     <h5 className="truncated-title font-title text-base large-desktop:text-xl">
--- src/pages/CategoryContent.tsx
+++ src/pages/CategoryContent.tsx
@@ -18,7 +18,7 @@ export const CategoryContent = () => {
 
             <div className="grid grid-cols-1 w-full mx-auto tablet:grid-cols-2 large-desktop:w-4/5">
 
-                {recentArticles.slice(0, 4).map(({ id, title, cover, contenido, publishedAt }) => {
+                {recentArticles.slice(0, 4).map(({ id, title, cover, contenido, publishedAt, slug }) => {
 
                     const formattedDate = new Intl.DateTimeFormat('es-ES', {
                         year: 'numeric',
@@ -30,14 +30,14 @@ export const CategoryContent = () => {
                         <div key={id} className="card m-3">
                             <div className="mobile:flex desktop:flex large-desktop:flex h-full">
                                 <div className="desktop:w-2/3 large-desktop:w-1/3">
-                                    <Link to={`/article/${title}`} className="no-underline text-inherit">
+                                    <Link to={`/article/${slug}`} className="no-underline text-inherit">
                                         <img src={cover} className="object-cover rounded-sm h-48 large-desktop:h-52 w-full" alt="foto" />
                                     </Link>
                                 </div>
 
                                 <div className="w-full large-desktop:w-4/5">
                                     <div className="card-body">
-                                        <Link to={`/article/${title}`} className="no-underline text-inherit">
+                                        <Link to={`/article/${slug}`} className="no-underline text-inherit">
                                             <h5 className="truncated-title font-title text-base large-desktop:text-xl no-underline text-inherit">{title}</h5>
                                         </Link>
                                         <div className="truncated-text font-secondary text-sm desktop:text-base large-desktop:text-lg">
@@ -56,7 +56,7 @@ export const CategoryContent = () => {
 
             <div className="grid grid-cols-1 tablet:grid-cols-4 w-full large-desktop:w-4/5 mx-auto gap-10 p-3 large-desktop:mt-3 large-desktop:mb-10">
                 {
-                    recentArticles.slice(8, 12).map(({ id, title, cover, contenido, publishedAt }) => {
+                    recentArticles.slice(8, 12).map(({ id, title, cover, contenido, publishedAt,slug }) => {
 
                         const formattedDate = new Intl.DateTimeFormat('es-ES', {
                             year: 'numeric',
@@ -66,7 +66,7 @@ export const CategoryContent = () => {
 
                         return (
                             <div key={id}>
-                                <Link to={`/article/${title}`} className="no-underline text-inherit">
+                                <Link to={`/article/${slug}`} className="no-underline text-inherit">
                                     <div className="card w-full">
                                         <img src={cover} className="object-cover rounded-sm h-48 large-desktop:h-52" alt="foto" />
                                         <div className="card-body">
